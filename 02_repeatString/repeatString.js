e4443c50e7beb7f52a947dd3fc855d546101f05b
jpaolosy88 <95204492+jpaolosy88@users.noreply.github.com>
2022-11-26 23:04:38 +0800

    top javascript-exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, qty) {
+  if (qty < 0) {
+    return "ERROR";
+  }
+  let result = "";
+  for (let i = 0; i < qty; i++) {
+    result += word;
+  }
+  return result;
 };
 
 // Do not edit below this line
