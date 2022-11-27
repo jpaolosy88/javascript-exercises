516d638e6d66861d3953211fa9652c3fc21f144f
jpaolosy88 <95204492+jpaolosy88@users.noreply.github.com>
2022-11-27 23:37:39 +0800

    top javascript exercises 5,6,7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let numInit = num1;
+  let numFinal = num2;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 != "number" ||
+    typeof num2 != "number"
+  ) {
+    return "ERROR";
+  } else {
+    if (num1 > num2) {
+      numInit = num2;
+      numFinal = num1;
+    }
+    return ((numFinal - numInit + 1) * (numInit + numFinal)) / 2;
+  }
 };
 
 // Do not edit below this line
