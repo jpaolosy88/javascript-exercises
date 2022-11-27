aae9d32ed014e5f01b4e69f6a40a83ee65b2672b
jpaolosy88 <95204492+jpaolosy88@users.noreply.github.com>
2022-11-27 11:00:06 +0800

    top javascript exercises 03/04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...items) {
+  for (let item of items) {
+    let toRemove = arr.indexOf(item);
+    if (toRemove != -1) {
+      arr.splice(toRemove, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
